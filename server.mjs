5d272c6761c8646053c1a0834f5d0d1b6eb32b36
aqsafaisal34 <aqsay.faisal34@gmail.com>
2023-08-07 23:40:13 +0500

    Add database

--- server.mjs
+++ server.mjs
@@ -2,103 +2,145 @@ import express from "express";
 import path from "path";
 import { nanoid } from "nanoid";
 const __dirname = path.resolve();
+import { ObjectId } from "mongodb";
+
+// set on environment variable
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 app.use(express.json());
+import { MongoClient } from "mongodb";
 
 const PORT = process.env.PORT || 3000;
 
-// create dummy post then replace it with mongodb database
-let posts = [
-  {
-    id: nanoid(),
-    title: "first post",
-    text: "This is dummy post",
-  },
-];
+const mongodbURI = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.iunhwh0.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(mongodbURI);
+async function run() {
+  try {
+    await client.connect();
+    console.log("succesfully connected to Atlas");
+  } catch (err) {
+    console.log(err.stack);
+    await client.close();
+    process.exit(1);
+  }
+}
+run().catch(console.dir);
 
-app.post("/post", (req, res, next) => {
+process.on("SIGINT", async () => {
+  console.log("app is terminaring");
+  await client.close();
+});
+
+const database = client.db("crudapp");
+const postsCollection = database.collection("posts");
+
+app.post("/post", async (req, res, next) => {
   if (!req.body.title || !req.body.text) {
     return res.status(400).json({
       error: "title and text are required",
     });
   }
-  posts.push({
+  // add document into database
+
+  const post = {
     id: nanoid(),
     title: req.body.title,
     text: req.body.text,
-  });
-  res.send({
-    message: "Post added",
-    posts: posts,
-  });
-});
-
-app.get("/posts", (req, res, next) => {
-  res.send({
-    posts: posts,
-  });
-});
-
-app.get("/post/:id", (req, res, next) => {
-  let id = req.params.id;
-  const postFound = posts.find((post) => post.id == id);
-  if (!postFound) {
-    return res.status(404).json({
-      error: "Post not found",
-    });
-  } else {
+  };
+  try {
+    const result = await postsCollection.insertOne(post);
     res.send({
-      post: postFound,
-      message: "Post found",
+      message: "Post added",
+      posts: post,
     });
+  } catch (err) {
+    console.log(err);
   }
 });
 
-app.put('/post/:postId', (req, res, next) => {
+app.get("/posts", async (req, res, next) => {
+  try {
+    const posts = await postsCollection.find({}).toArray();
+    res.send({
+      posts: posts,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+});
 
-  if (!req.params.postId
-      || !req.body.text
-      || !req.body.title) {
-      res.status(403).send(`example put body: 
+app.get("/post/:id", async (req, res, next) => {
+  try {
+    const result = await postsCollection.findOne({
+      _id: new ObjectId(req.params.id),
+    });
+    res.send(result);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+app.put("/post/:postId", async (req, res, next) => {
+  // if (!ObjectId.isValid(req.params.postId)) {
+  //   res.status(403).send(`Invalid post id`);
+  //   return;
+  // }
+
+  if (!req.body.text && !req.body.title) {
+    res.status(403).send(`required parameter missing, example put body: 
       PUT     /api/v1/post/:postId
       {
           title: "updated title",
           text: "updated text"
       }
-      `)
+      `);
+  }
+  let dataToBeUpdated = {};
+
+  if (req.body.title) {
+    dataToBeUpdated.title = req.body.title;
+  }
+  if (req.body.text) {
+    dataToBeUpdated.text = req.body.text;
   }
 
-  for (let i = 0; i < posts.length; i++) {
-      if (posts[i].id === req.params.postId) {
-          posts[i] = {
-              id: req.params.postId,
-              text: req.body.text,
-              title: req.body.title,
-          }
-          res.send('post updated with id ' + req.params.postId);
-          return;
+  try {
+    const updateResponse = await postsCollection.updateOne(
+      {
+        _id: new ObjectId(req.params.postId),
+      },
+      {
+        $set: dataToBeUpdated,
       }
+    );
+    console.log("updateResponse: ", updateResponse);
+
+    res.send("post updated");
+  } catch (e) {
+    console.log("error inserting mongodb: ", e);
+    res.status(500).send("server error, please try later");
   }
-  res.send('post not found with id ' + req.params.postId);
-})
+});
 
-app.delete('/post/:postId', (req, res, next) => {
-
-  if (!req.params.postId) {
-      res.status(403).send(`post id must be a valid id`)
+app.delete("/post/:postId", async (req, res, next) => {
+  if (!ObjectId.isValid(req.params.postId)) {
+    res.status(403).send(`Invalid post id`);
+    return;
   }
 
-  for (let i = 0; i < posts.length; i++) {
-      if (posts[i].id === req.params.postId) {
-          posts.splice(i, 1)
-          res.send('post deleted with id ' + req.params.postId);
-          return;
-      }
+  try {
+    const deleteResponse = await postsCollection.deleteOne({
+      _id: new ObjectId(req.params.postId),
+    });
+    console.log("deleteResponse: ", deleteResponse);
+    res.send("post deleted");
+  } catch (e) {
+    console.log("error deleting mongodb: ", e);
+    res.status(500).send("server error, please try later");
   }
-  res.send('post not found with id ' + req.params.postId);
-})
-
+});
 
 app.use(express.static(path.join(__dirname, "public")));
 
